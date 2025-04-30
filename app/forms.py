d9a08c3efc9338449a42e0f1044864320db84c7c
Sean Spence <seanspence2019@gmail.com>
2025-04-30 01:31:34 -0500

    Updated forms, models, views,config and __init__.py

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,19 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField,FileField,SubmitField
+from wtforms.validators import InputRequired
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+
+class RegisterForm(FlaskForm):
+    Fullname = StringField('Title', validators=[InputRequired()])
+    username = StringField('Title', validators=[InputRequired()])
+    password = StringField('Title', validators=[InputRequired()])
+    email = StringField('Title', validators=[InputRequired()])
+    photo = FileField('poster', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
+    submit = SubmitField('Register')
+    
+    
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
--- app/views.py
+++ app/views.py
@@ -5,8 +5,15 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
+from app import app, db
 from flask import render_template, request, jsonify, send_file
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.security import check_password_hash
+from werkzeug.utils import secure_filename
+from app.models import Users
+from app.forms import RegisterForm
+from app.forms import LoginForm
+import re
 import os
 
 
@@ -43,6 +50,102 @@ def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
+
+@app.route('/api/register',methods=['POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        fullname = form.fullname.data.strip()
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        photo_file = form.photo.data
+
+        names = fullname.split(" ", 1)
+        first_name = names[0]
+        last_name = names[1] if len(names) > 1 else ''
+
+        filename = secure_filename(photo_file.filename)
+        photo_path = os.path.join('uploads', filename)
+        photo_file.save(photo_path)
+
+        new_user = Users(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email,
+            photo=filename
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({
+            'error': False,
+            'message': 'User registered successfully',
+            'user': {
+                'id': new_user.id,
+                'name': new_user.name,
+                'username': new_user.username,
+                'email': new_user.email,
+                'date_joined': new_user.date_joined
+            }
+        }), 201
+    else:
+        return jsonify({
+            'error': True,
+            'message': 'Form validation failed',
+            'errors': form_errors(form)
+        }), 400
+
+
+
+
+@app.route('/api/auth/login',methods=['POST'])
+def login():
+    return 1
+
+
+
+
+
+@app.route('/api/auth/logout',methods=['POST'])
+
+
+
+@app.route('/api/profiles',methods=['GET','POST'])
+
+
+
+@app.route('/api/profiles/{profile_id}',methods=['GET'])
+
+
+
+@app.route('/api/profiles/{user_id}/favourite',methods=['POST'])
+
+
+
+@app.route('/api/profiles/matches/{profile_id}',methods=['GET'])
+
+
+
+@app.route('/api/search',methods=['GET'])
+
+
+
+@app.route('/api/users/{user_id}',methods=['GET'])
+
+
+
+@app.route('/api/users/{user_id}/favourites',methods=['GET'])
+
+
+
+@app.route('/api/users/favourties/{N}',methods=['GET'])
+
+
 
 
 @app.after_request
