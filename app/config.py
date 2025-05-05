ba00908d2c7e619b38148668183c0965ac6f8862
Sean Spence <seanspence2019@gmail.com>
2025-05-05 17:16:54 -0500

    Updated config files

--- app/config.py
+++ app/config.py
@@ -1,12 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # load environment variables from .env if it exists.
+load_dotenv()  # Load environment variables from .env if it exists.
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
+    UPLOAD_FOLDER = os.path.join(basedir, 'static', 'uploads')  # Use absolute path
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/views.py
+++ app/views.py
@@ -47,6 +47,10 @@ def catch_all(path):
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 
 ###
